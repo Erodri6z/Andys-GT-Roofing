6255d8d1ca30c7db2e095cd2e015b5b5a7dc28f3
Erodri6z <erikrodriguez2417@gmail.com>
2024-04-28 19:21:51 -0500

    add a services component

---
+++ src/components/service/services.jsx
@@ -0,0 +1,30 @@
+
+
+function Services() {
+  return (
+    <>
+    <div>
+      <h3>Services</h3>
+      <div>
+      <div>
+          <h4>Technos</h4>
+          <p>Techno stuff Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laudantium vel reprehenderit, modi cupiditate necessitatibus temporibus quae nesciunt dolorum voluptas harum qui, tempora quibusdam ullam ipsam, nulla alias maiores beatae?</p>
+        </div>
+        <div>
+          <h4>Sidings</h4>
+          <p>Techno stuff Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laudantium vel reprehenderit, modi cupiditate necessitatibus temporibus quae nesciunt dolorum voluptas harum qui, tempora quibusdam ullam ipsam, nulla alias maiores beatae?</p>
+        </div>
+        <div>
+          <h4>Windows</h4>
+          <p>Techno stuff Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laudantium vel reprehenderit, modi cupiditate necessitatibus temporibus quae nesciunt dolorum voluptas harum qui, tempora quibusdam ullam ipsam, nulla alias maiores beatae?</p>
+        </div>
+        <div>
+          <h4>And Much More</h4>
+          <p>Techno stuff Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laudantium vel reprehenderit, modi cupiditate necessitatibus temporibus quae nesciunt dolorum voluptas harum qui, tempora quibusdam ullam ipsam, nulla alias maiores beatae?</p>
+        </div>
+      </div>
+    </div>
+    </>
+  )
+}
+
