49783b16e033026269b14cd77edcfb96af09c28e
Erodri6z <erikrodriguez2417@gmail.com>
2024-05-02 20:36:45 -0500

    add more css to services page

--- src/components/service/services.jsx
+++ src/components/service/services.jsx
@@ -4,21 +4,21 @@ function Services() {
   return (
     <>
     <div className="service">
-      <h3>Services</h3>
-      <div>
-      <div>
+      <h2 className="services-title">Services</h2>
+      <div className="services-container">
+        <div className="service-div">
           <h4>Roofing</h4>
           <p>Techno stuff Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laudantium vel reprehenderit, modi cupiditate necessitatibus temporibus quae nesciunt dolorum voluptas harum qui, tempora quibusdam ullam ipsam, nulla alias maiores beatae?</p>
         </div>
-        <div>
+        <div className="service-div">
           <h4>Sidings</h4>
           <p>Techno stuff Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laudantium vel reprehenderit, modi cupiditate necessitatibus temporibus quae nesciunt dolorum voluptas harum qui, tempora quibusdam ullam ipsam, nulla alias maiores beatae?</p>
         </div>
-        <div>
+        <div className="service-div">
           <h4>Windows</h4>
           <p>Techno stuff Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laudantium vel reprehenderit, modi cupiditate necessitatibus temporibus quae nesciunt dolorum voluptas harum qui, tempora quibusdam ullam ipsam, nulla alias maiores beatae?</p>
         </div>
-        <div>
+        <div className="service-div">
           <h4>And Much More</h4>
           <p>Techno stuff Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laudantium vel reprehenderit, modi cupiditate necessitatibus temporibus quae nesciunt dolorum voluptas harum qui, tempora quibusdam ullam ipsam, nulla alias maiores beatae?</p>
         </div>
