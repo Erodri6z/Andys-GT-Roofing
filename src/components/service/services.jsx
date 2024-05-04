bb57146db785b4fc7ae5e45c1005bef3119efc9d
Erodri6z <erikrodriguez2417@gmail.com>
2024-05-04 13:47:02 -0500

    add some images

--- src/components/service/services.jsx
+++ src/components/service/services.jsx
@@ -1,12 +1,14 @@
 import "./service.css"
+import roofing from "../../assets/Roofing.webp"
 
 function Services() {
+  
   return (
     <>
     <div className="service">
       <h2 className="services-title">Services</h2>
       <div className="services-container">
-        <div className="service-div">
+        <div className="service-div" style={{backgroundImage: `url(${roofing})`}}>
           <h4>Roofing</h4>
           <p>Techno stuff Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laudantium vel reprehenderit, modi cupiditate necessitatibus temporibus quae nesciunt dolorum voluptas harum qui, tempora quibusdam ullam ipsam, nulla alias maiores beatae?</p>
         </div>
