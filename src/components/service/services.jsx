838984ef34bf82b411fe2d233c91c1118e4fc02f
Erodri6z <erikrodriguez2417@gmail.com>
2024-05-04 14:12:00 -0500

    add some css to the =background images

--- src/components/service/services.jsx
+++ src/components/service/services.jsx
@@ -12,19 +12,19 @@ function Services() {
     <div className="service">
       <h2 className="services-title">Services</h2>
       <div className="services-container">
-        <div className="service-div" style={{backgroundImage: `url(${roofing})`}}>
+        <div className="service-div" style={{backgroundImage: `url(${roofing})`, backgroundPositionX: "center", backgroundSize: "cover" }}>
           <h4>Roofing</h4>
           <p>Techno stuff Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laudantium vel reprehenderit, modi cupiditate necessitatibus temporibus quae nesciunt dolorum voluptas harum qui, tempora quibusdam ullam ipsam, nulla alias maiores beatae?</p>
         </div>
-        <div className="service-div" style={{backgroundImage: `url(${sidings})`}}>
+        <div className="service-div" style={{backgroundImage: `url(${sidings})`, backgroundPositionX: "center", backgroundSize: "cover" }}>
           <h4>Sidings</h4>
           <p>Techno stuff Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laudantium vel reprehenderit, modi cupiditate necessitatibus temporibus quae nesciunt dolorum voluptas harum qui, tempora quibusdam ullam ipsam, nulla alias maiores beatae?</p>
         </div>
-        <div className="service-div" style={{backgroundImage: `url(${windows})`}}>
+        <div className="service-div" style={{backgroundImage: `url(${windows})`, backgroundPositionX: "center", backgroundSize: "cover" }}>
           <h4>Windows</h4>
           <p>Techno stuff Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laudantium vel reprehenderit, modi cupiditate necessitatibus temporibus quae nesciunt dolorum voluptas harum qui, tempora quibusdam ullam ipsam, nulla alias maiores beatae?</p>
         </div>
-        <div className="service-div" style={{backgroundImage: `url(${others})`}}>
+        <div className="service-div" style={{backgroundImage: `url(${others})`, backgroundPositionX: "center", backgroundSize: "cover" }}>
           <h4>And Much More</h4>
           <p>Techno stuff Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laudantium vel reprehenderit, modi cupiditate necessitatibus temporibus quae nesciunt dolorum voluptas harum qui, tempora quibusdam ullam ipsam, nulla alias maiores beatae?</p>
         </div>
