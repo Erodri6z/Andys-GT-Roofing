504246dc6839a9ea9c5919b2b7c29bca30fa4ab6
Erodri6z <erikrodriguez2417@gmail.com>
2024-05-07 20:27:09 -0500

    add better css to service

--- src/components/service/services.jsx
+++ src/components/service/services.jsx
@@ -12,10 +12,12 @@ function Services() {
     <div className="service">
       <h2 className="services-title">Services</h2>
       <div className="services-container">
-        <div className="service-div" style={{backgroundImage: `url(${roofing})`, backgroundPositionX: "center", backgroundSize: "cover" }}>
-          <h4>Roofing</h4>
-          <p>Techno stuff Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laudantium vel reprehenderit, modi cupiditate necessitatibus temporibus quae nesciunt dolorum voluptas harum qui, tempora quibusdam ullam ipsam, nulla alias maiores beatae?</p>
-        </div>
+      <div class="service-div" style={{backgroundImage: `url(${roofing})`, backgroundPositionX: "center", backgroundSize: "cover" }}>
+      <div class="text">
+        <h4>Roofing</h4>
+        <p>Techno stuff Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laudantium vel reprehenderit, modi cupiditate necessitatibus temporibus quae nesciunt dolorum voluptas harum qui, tempora quibusdam ullam ipsam, nulla alias maiores beatae?</p>
+      </div>
+    </div>
         <div className="service-div" style={{backgroundImage: `url(${sidings})`, backgroundPositionX: "center", backgroundSize: "cover" }}>
           <h4>Sidings</h4>
           <p>Techno stuff Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos laudantium vel reprehenderit, modi cupiditate necessitatibus temporibus quae nesciunt dolorum voluptas harum qui, tempora quibusdam ullam ipsam, nulla alias maiores beatae?</p>
